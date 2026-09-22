1a98f4f19e5387ab1a5baadcaed4591416619764
Frandy Pratama <frandypratama549@gmail.com>
2026-09-22 10:47:43 +0700

    fix vercel express deployment

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,3 @@
 import app from "./app.js";
 
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-  console.log(`Server berjalan di http://localhost:${PORT}`);
-});
+export default app;
