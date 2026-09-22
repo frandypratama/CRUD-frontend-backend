1499356f98102d1ded8007a98ccc43468a6b6062
Frandy Pratama <frandypratama549@gmail.com>
2026-09-22 10:54:19 +0700

    update

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,7 @@
 import app from "./app.js";
 
-export default app;
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server berjalan di http://localhost:${PORT}`);
+});
